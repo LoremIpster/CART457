d027b8e0e823b8ab1674d2db958fd3b4660efc8b
Simon-Albert Boudreault <saboudreault@gmail.com>
2017-10-23 17:19:12 -0400

    OF build update
    * replaced github version with another local version
    
    + timer is functional
    + integrated script that make the timer only tick when player moves
    + added fog and grass
    + added outdoor soundscape and grass footsteps
    
    ? still unsure about the spawn. i made a scene with a house at the beginning, and the first choice is to actually access the open field.
    
    ? openGL works! but lags. still unsure if i'm using it right, but it helps for testing pruposes (easy to share)

---
+++ openfield/Assets/Scripts/Timer.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class Timer : MonoBehaviour {
+
+	private CharacterController controller;
+	private FirstPersonController fpController;
+
+	private float xPos;
+	private float zPos;
+
+	public float timeRemaining = 60;
+	public Text gameOverText;
+	public Text timerText;
+
+
+
+	// Use this for initialization
+	void Start () {
+		xPos = transform.position.x;
+		zPos = transform.position.z;
+
+		controller = GetComponent<CharacterController> ();
+		fpController = GetComponent<FirstPersonController> ();
+		timerText.text = "Il ne te reste que " + timeRemaining.ToString("N0") + " secondes à vivre.";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (xPos != transform.position.x || zPos != transform.position.z) {
+			timeRemaining -= Time.deltaTime;
+			timerText.text = "Il ne te reste que " + timeRemaining.ToString("N0") + " secondes à vivre.";
+
+			xPos = transform.position.x;
+			zPos = transform.position.z;
+
+			if(timeRemaining <= 0){
+				gameOverText.text = "c'est fini";
+				timerText.enabled = false;
+				controller.enabled = false;
+				fpController.enabled = false;
+			}
+		}
+	}
+
+	// for debug only
+	void OnGUI(){
+	
+		if (timeRemaining > 0) {
+			//GUI.Label (new Rect (100, 100, 200, 100), "Time Remaining : " + timeRemaining);
+		//	timerText.text = "Il ne te reste que " + timeRemaining + "secondes à vivre.";
+		} else {
+			//GUI.Label (new Rect (100, 100, 100, 100), "Time's up!");
+
+		}
+
+	}
+}
