981c2fbadad25477bb1cd0ec232e7b0132fce801
saboudreault <saboudreault@gmail.com>
2017-11-05 21:59:32 -0500

    OF0010
    + new path leading to the tower
    + moved around the sun to fit the tower
    + forest has been revamped.  hundreds of trees removed but the density feeling is still there
    + mountain has been smooted out
    
    ? still working on a stillness script. I want stuff to happen when the player stops
    ? what about those TVs? still hesitant to remove them, but cannot find a good use yet

---
+++ openfield/Assets/Scripts/Stillness.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stillness : MonoBehaviour {
+	
+	public Rigidbody rb;
+	Vector3 vel;
+
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+		vel = rb.velocity;
+	}
+	
+	void FixedUpdate () {
+		Debug.Log (rb.velocity);
+		if (rb.velocity.x == 0 && rb.velocity.z == 0) {
+			Debug.Log ("vel is " + vel);
+		} else if (vel.x > 0 && vel.y > 0){
+			Debug.Log ("MOVING");
+		}
+		
+	}
+}
